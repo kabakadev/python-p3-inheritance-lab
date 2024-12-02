1b27f3aa9dd5264f6418e831d8c86c212bfb9035
Ian Kabaka <iankabaka1@gmail.com>
2024-12-02 23:25:23 +0300

    passing tests for p3 inheritance

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
 from user import User
+knowledge = []
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
     
-    def learn(self):
+    def learn(self,any_string):
+        self.knowledge.append(any_string)
         pass
